4bf66613ea9129c9db7e7c70a5f799dc7d9513fc
Søren Døssing <netcom@sauber.net>
2026-05-19 18:24:41 +0200

    Animate movements in ascii window

--- loop.ts
+++ loop.ts
@@ -1,6 +1,42 @@
 import { centerForce } from "./src/center.ts";
 import { implodeForce } from "./src/implode.ts";
 import { Node } from "./src/node.ts";
+import {
+  Canvas,
+  CharCanvas,
+  drawFrame,
+  drawLabelCentered,
+} from "jsr:@sauber/ansi-draw";
+import { delay } from "jsr:@std/async/delay";
+
+// const terminal = new CharCanvas();
+
+function drawNode(terminal: Canvas, tree: Node): void {
+  if ("label" in tree) {
+    const label = tree.label as string;
+    const x = tree.x;
+    const y = tree.y;
+    drawLabelCentered(terminal, x, y, label);
+  } else {
+    if (tree.width >= 2 && tree.height >= 2) {
+      drawFrame(terminal, tree.left, tree.bottom, tree.width, tree.height);
+    }
+  }
+
+  tree.children.forEach((c) => drawNode(terminal, c));
+}
+
+let height = 0;
+function drawTree(tree: Node): void {
+  const terminal = new CharCanvas();
+  drawNode(terminal, tree);
+  const output = terminal.toString();
+  const up: string = "\u001b[" + height + "A";
+  const reset: string = "\u001b[0J";
+  console.log(up + reset + output);
+  height = output.split("\n").length;
+  // console.log("Height: " + height);
+}
 
 // Display array with few digits
 const nums = (values: number[]): number[] =>
@@ -18,15 +54,19 @@ const debug = (i: number, node: Node): void =>
   );
 
 // Example of box seeking center of canvas
-const canvas = new Node([0, 0, 100, 100]);
+const canvas = new Node([1, 1, 80, 24]);
 const node = canvas.addNode();
+// const text = node.addText("Hello, World!");
 debug(0, node);
 
-for (let i = 0; i < 200; i++) {
+for (let i = 0; i < 20; i++) {
   centerForce(canvas);
-  debug(i, node);
+  // debug(i, node);
   implodeForce(canvas);
-  debug(i, node);
+  // debug(i, node);
   canvas.move();
   // debug(node);
+  drawTree(canvas);
+  await delay(100);
 }
+console.log(node);
